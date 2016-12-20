a600ac2a9263e2553ab29be8d58a57d87eb25c17
Oluwadamilare Olusakin <Oluwadamilare_olusakin@yahoo.com>
2016-12-20 11:31:24 +0100

    Add Static pages controller, users controller, Customer model,Order model and Meal model

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def meals
+  end
+
+  def about
+  end
+
+  def how
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
+  get 'user/login'
+
+  get 'static_pages/index'
+
+  get 'static_pages/meals'
+
+  get 'static_pages/about'
+
+  get 'static_pages/how'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
