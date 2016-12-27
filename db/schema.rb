40cee848aff5b5832fa20d813a7d578a5a132e90
Oluwadamilare Olusakin <Oluwadamilare_olusakin@yahoo.com>
2016-12-27 12:12:17 +0100

    Add password validation to customers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220103000) do
+ActiveRecord::Schema.define(version: 20161227105129) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20161220103000) do
     t.integer  "customer_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
   end
 
   create_table "meals", force: :cascade do |t|
