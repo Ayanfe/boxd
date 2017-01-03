22eb10d6589a585bf51b4b4f56bbfa6ca246090b
Oluwadamilare Olusakin <Oluwadamilare_olusakin@yahoo.com>
2017-01-03 19:32:03 +0100

    Revert "Revert "Save point 2.6""
    This reverts commit 1e80c71e4906341cd3e0ace3b20090d2e661d357.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'static_pages#index'
 
   get '/index', to: 'static_pages#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103171723) do
+ActiveRecord::Schema.define(version: 20170103181248) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
