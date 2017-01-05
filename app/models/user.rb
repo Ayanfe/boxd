f779004e0f48b19250ffb6666ebcd64b03d43ba7
Oluwadamilare Olusakin <Oluwadamilare_olusakin@yahoo.com>
2017-01-05 22:58:28 +0100

    Start styling meals page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :first_name, presence:true, length:{maximum:20}
   validates :last_name, presence:true, length:{maximum:20}
   has_one :address
